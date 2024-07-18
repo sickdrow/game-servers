4d2287574e43a31c1f7cbbf0e6cedb1779f2fe31
Grzesiek <sickdrow@gmail.com>
2024-07-18 09:31:47 +0200

    db

---
+++ otsrv/db/initial.sql
@@ -0,0 +1,10 @@
+USE dev;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
+
+INSERT INTO users (username, email) VALUES
+('otsrv', 'otsrv@localhost'),
